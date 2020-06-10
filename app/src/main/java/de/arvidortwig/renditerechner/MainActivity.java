88afcc2e6e8917334df065126e27e1c7e9e10a71
Arvid Ortwig <email@arvidortwig.de>
2020-06-10 14:22:59 +0200

    inserted switch month/year

--- app/src/main/java/de/arvidortwig/renditerechner/MainActivity.java
+++ app/src/main/java/de/arvidortwig/renditerechner/MainActivity.java
@@ -5,11 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button calc = (Button) findViewById(R.id.calc);
+        final Switch monatjahr = (Switch) findViewById(R.id.switch1_monat_jahr);
+
+
 
 
         calc.setOnClickListener(new View.OnClickListener() {
@@ -35,23 +44,37 @@ public class MainActivity extends AppCompatActivity {
                 EditText grundsteuer = (EditText) findViewById(R.id.editTextNumber_Grundsteuer);
                 TextView einnahmen = (TextView) findViewById(R.id.textView_Einnahmen);
 
-                int int_groesse_m2=Integer.parseInt(groesse_m2.getText().toString());
-                int int_kaltmiete_m2=Integer.parseInt(kaltmiete_m2.getText().toString());
-                int int_nebenkosten=Integer.parseInt(nebenkosten.getText().toString());
-                int int_nebenkosten_prozent=Integer.parseInt(nebenkosten_prozent.getText().toString());
-                int int_stellplatz=Integer.parseInt(stellplatz.getText().toString());
-                int int_grundsteuer=Integer.parseInt(grundsteuer.getText().toString());
+                int int_groesse_m2,int_kaltmiete_m2,int_nebenkosten,int_nebenkosten_prozent,int_stellplatz,int_grundsteuer,int_kaltmiete_gesamt,int_einnahmen;
+
+                int_groesse_m2=Integer.parseInt(groesse_m2.getText().toString());
+                int_kaltmiete_m2=Integer.parseInt(kaltmiete_m2.getText().toString());
+                int_nebenkosten=Integer.parseInt(nebenkosten.getText().toString());
+                int_nebenkosten_prozent=Integer.parseInt(nebenkosten_prozent.getText().toString());
+                int_stellplatz=Integer.parseInt(stellplatz.getText().toString());
+                int_grundsteuer=Integer.parseInt(grundsteuer.getText().toString());
 
 
-                int int_kaltmiete_gesamt=int_groesse_m2*int_kaltmiete_m2;
-                int int_einnahmen=int_kaltmiete_gesamt+int_stellplatz-int_grundsteuer-(int_nebenkosten-(int_nebenkosten*int_nebenkosten_prozent/100));
+                int_kaltmiete_gesamt=int_groesse_m2*int_kaltmiete_m2;
+                int_einnahmen=int_kaltmiete_gesamt+int_stellplatz-int_grundsteuer-(int_nebenkosten-(int_nebenkosten*int_nebenkosten_prozent/100));
 
-                kaltmiete_gesamt_ergebnis.setText(Integer.toString(int_kaltmiete_gesamt));
-                einnahmen.setText(Integer.toString((int_einnahmen)));
-
-
+                //kaltmiete_gesamt_ergebnis.setText(Integer.toString(int_kaltmiete_gesamt));
+                //einnahmen.setText(Integer.toString((int_einnahmen)));
 
 
+                        if(monatjahr.getText().toString().equals("on"))
+                        {
+                            int_kaltmiete_gesamt= int_kaltmiete_gesamt*12;
+                            int_einnahmen =  int_einnahmen*12;
+                            kaltmiete_gesamt_ergebnis.setText(Integer.toString(int_kaltmiete_gesamt));
+                            einnahmen.setText(Integer.toString((int_einnahmen)));
+
+                        }
+
+                        else if(monatjahr.getText().toString().equals("off"))
+                        {
+                            kaltmiete_gesamt_ergebnis.setText(Integer.toString(int_kaltmiete_gesamt));
+                            einnahmen.setText(Integer.toString((int_einnahmen)));
+                        }
 
             }
 
